7f8136ea26ba7ba0bdee096af5d06186f92efd90
PersesTitan <ToramPersesTitan@gmail.com>
2022-07-05 21:26:27 +0900

    생성, 삭제 로직 생성

--- src/main/java/com/community/soccer/repository/MemberRepository.java
+++ src/main/java/com/community/soccer/repository/MemberRepository.java
@@ -1,10 +1,24 @@
 package com.community.soccer.repository;
 
+import com.community.soccer.domain.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+
+import javax.persistence.EntityManager;
 
 @Controller
 @RequiredArgsConstructor
 public class MemberRepository {
+
+    private final EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public void remove(Member member) {
+        em.remove(member);
+    }
+
 
 }
