b5c73ab2e747adc2e12a6489b9d8adf28adc78fc
PersesTitan <ToramPersesTitan@gmail.com>
2022-07-09 11:50:41 +0900

    equals 추가

--- src/main/java/com/community/soccer/domain/member/Member.java
+++ src/main/java/com/community/soccer/domain/member/Member.java
@@ -1,12 +1,17 @@
 package com.community.soccer.domain.member;
 
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.Hibernate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 @Getter
@@ -30,5 +35,19 @@ public class Member {
     //생성 로직
     public static Member createMember(String loginId, String nickName) {
         return new Member(loginId, nickName);
+    }
+
+    //equals
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Member member = (Member) o;
+        return id != null && Objects.equals(id, member.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
     }
 }
