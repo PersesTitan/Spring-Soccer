9c221f9bda6c7a0156eaabfa6d5a2442c6fe408c
PersesTitan <ToramPersesTitan@gmail.com>
2022-07-09 11:51:59 +0900

    equals 추가

--- src/main/java/com/community/soccer/domain/player/Player.java
+++ src/main/java/com/community/soccer/domain/player/Player.java
@@ -5,8 +5,10 @@ import com.community.soccer.domain.member.Member;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.Hibernate;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -29,5 +31,18 @@ public class Player {
     //생성 로직
     public static Player createPlayer(Position position) {
         return new Player(position);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Player player = (Player) o;
+        return id != null && Objects.equals(id, player.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
     }
 }
