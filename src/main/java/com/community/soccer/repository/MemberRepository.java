9b272fdc2a178e33ed861c177a3ada87f07be8e3
PersesTitan <ToramPersesTitan@gmail.com>
2022-07-09 11:50:58 +0900

    맴버 조회 메소드 추가

--- src/main/java/com/community/soccer/repository/MemberRepository.java
+++ src/main/java/com/community/soccer/repository/MemberRepository.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -25,5 +26,23 @@ public class MemberRepository {
     public void update(Member member, MemberEditDao dto) {
         member.setLoginId(dto.loginId());
         member.setNickName(dto.nickName());
+    }
+
+    //아이디 조회
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    //모든 맴버 리스트
+    public List<Member> findAll() {
+        return em.createQuery("SELECT M FROM Member AS M", Member.class)
+                .getResultList();
+    }
+
+    //닉네임 검색 로직
+    public List<Member> findSearch(String keyWord) {
+        return em.createQuery("SELECT M FROM Member AS M WHERE M.nickName LIKE :keyWord", Member.class)
+                .setParameter("keyWord", "%"+keyWord+"%")
+                .getResultList();
     }
 }
