4b74feface82c4e549e6b2a31338e5dc7fb6ef94
PersesTitan <ToramPersesTitan@gmail.com>
2022-07-09 11:50:27 +0900

    equals 추가

--- src/main/java/com/community/soccer/domain/game/Game.java
+++ src/main/java/com/community/soccer/domain/game/Game.java
@@ -5,11 +5,13 @@ import com.community.soccer.domain.player.Player;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.Hibernate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Entity @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -29,4 +31,17 @@ public class Game {
 
     @OneToMany(cascade = CascadeType.ALL)
     private List<Player> players = new ArrayList<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Game game = (Game) o;
+        return id != null && Objects.equals(id, game.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
