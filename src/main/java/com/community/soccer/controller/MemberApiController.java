74d448ff1d254ed18464005b4c3c1f5e0e0b3712
PersesTitan <torampersesTitan@gmail.com>
2022-07-19 20:19:19 +0900

    add find, edit, delete, search

--- src/main/java/com/community/soccer/controller/MemberApiController.java
+++ src/main/java/com/community/soccer/controller/MemberApiController.java
@@ -2,15 +2,20 @@ package com.community.soccer.controller;
 
 import com.community.soccer.domain.member.Member;
 import com.community.soccer.domain.member.dao.MemberCreateDao;
+import com.community.soccer.domain.member.dao.MemberEditDao;
+import com.community.soccer.domain.member.dao.MemberFindDao;
+import com.community.soccer.domain.member.dao.MemberSearchDao;
 import com.community.soccer.domain.member.request.CreateMemberRequest;
+import com.community.soccer.domain.member.request.EditMemberRequest;
 import com.community.soccer.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.parameters.P;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/members")
@@ -29,6 +34,36 @@ public class MemberApiController {
         return new MemberCreateDao(id);
     }
 
+    @GetMapping("/{id}")
+    public MemberFindDao findMemberDao(@PathVariable Long id) {
+        Member member = memberService.findOne(id);
+        String nickName = member.getNickName();
+        LocalDateTime createDate = member.getCreateDate();
 
+        return new MemberFindDao(id, nickName, createDate);
+    }
+
+    @PatchMapping("/{id}")
+    public MemberEditDao editMemberDao(@PathVariable Long id,
+                                       @RequestBody @Valid EditMemberRequest editMemberRequest) {
+        Member member = memberService.update(id, editMemberRequest);
+        String nickName = member.getNickName();
+        LocalDateTime createDate = member.getCreateDate();
+        String loginId = member.getLoginId();
+
+        return new MemberEditDao(id, loginId, nickName, createDate);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMemberDao(@PathVariable Long id) {
+        memberService.remove(id);
+    }
+
+    @GetMapping
+    public MemberSearchDao searchDao(HttpServletRequest request) {
+        String keyword = request.getParameter("keyword");
+        List<Member> members = memberService.findSearch(keyword);
+        return new MemberSearchDao(members);
+    }
 
 }
