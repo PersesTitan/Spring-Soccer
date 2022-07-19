845a13cd608681875ed75050a2e8b8cf06be25d8
PersesTitan <torampersesTitan@gmail.com>
2022-07-19 20:18:33 +0900

    add update, remove, findOne, findSearch

--- src/main/java/com/community/soccer/service/MemberService.java
+++ src/main/java/com/community/soccer/service/MemberService.java
@@ -1,10 +1,14 @@
 package com.community.soccer.service;
 
 import com.community.soccer.domain.member.Member;
+import com.community.soccer.domain.member.dao.MemberEditDao;
+import com.community.soccer.domain.member.request.EditMemberRequest;
 import com.community.soccer.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -17,5 +21,29 @@ public class MemberService {
     public Long save(Member member) {
         memberRepository.save(member);
         return member.getId();
+    }
+
+    @Transactional
+    public Member update(Long id, EditMemberRequest request) {
+        Member member = memberRepository.findOne(id);
+        String nickName = request.nickName();
+
+        member.setNickName(nickName);
+        return memberRepository.update(member, nickName);
+    }
+
+    @Transactional
+    public void remove(Long id) {
+        Member member = memberRepository.findOne(id);
+        memberRepository.remove(member);
+    }
+
+    public Member findOne(Long id) {
+        return memberRepository.findOne(id);
+    }
+
+    public List<Member> findSearch(String keyWord) {
+        if (keyWord == null || keyWord.isBlank()) return memberRepository.findAll();
+        else return memberRepository.findSearch(keyWord);
     }
 }
