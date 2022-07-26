1bec4e4cc59af22a1e9152e229c2366fac909839
PersesTitan <torampersesTitan@gmail.com>
2022-07-26 15:05:58 +0900

    setting JsonFormat

--- src/main/java/com/community/soccer/domain/game/request/GameCreateRequest.java
+++ src/main/java/com/community/soccer/domain/game/request/GameCreateRequest.java
@@ -1,10 +1,14 @@
 package com.community.soccer.domain.game.request;
 
 import com.community.soccer.domain.item.Position;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.time.LocalDateTime;
 import java.util.Map;
 
 public record GameCreateRequest(String title, String place, String region,
-                                LocalDateTime startDate, LocalDateTime endDate,
+                                @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+                                LocalDateTime startDate,
+                                @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+                                LocalDateTime endDate,
                                 Map<Position, Integer> requirePlayer, String description) { }
