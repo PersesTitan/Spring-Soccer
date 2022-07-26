02c42af5e0b5d1ea473dfc6b2319a0eb9122b00f
PersesTitan <torampersesTitan@gmail.com>
2022-07-26 15:06:03 +0900

    setting JsonFormat

--- src/main/java/com/community/soccer/domain/game/request/GameEditRequest.java
+++ src/main/java/com/community/soccer/domain/game/request/GameEditRequest.java
@@ -1,5 +1,10 @@
 package com.community.soccer.domain.game.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDateTime;
 
-public record GameEditRequest(LocalDateTime startDate, LocalDateTime endDate, String region) { }
+public record GameEditRequest(@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+                              LocalDateTime startDate,
+                              @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+                              LocalDateTime endDate, String region) { }
