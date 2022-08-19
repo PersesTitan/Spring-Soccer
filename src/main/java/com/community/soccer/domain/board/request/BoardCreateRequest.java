92dfc928c1ca91103a6a58e017679b57bb7aa5b4
PersesTitan <torampersesTitan@gmail.com>
2022-08-19 09:52:43 +0900

    update BoardCreateRequest delete memberId

--- src/main/java/com/community/soccer/domain/board/request/BoardCreateRequest.java
+++ src/main/java/com/community/soccer/domain/board/request/BoardCreateRequest.java
@@ -1,5 +1,3 @@
 package com.community.soccer.domain.board.request;
 
-import com.community.soccer.domain.member.Member;
-
-public record BoardCreateRequest(String title, String content, Long memberId) { }
+public record BoardCreateRequest(String title, String content) { }
